5e6eddcb41657f65abcc679894e4c173b96d8a19
LocNguyen48 <23520860@gm.uit.edu.vn>
2024-09-13 22:46:25 +0700

    add changes

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const inputBox = document.getElementById("input-box");
 const listContainer = document.getElementById("List-container");
 function addTask(){
@@ -39,46 +38,4 @@ inputBox.addEventListener("keyup", function(event) {
       event.preventDefault();
       addTask();
     }
-=======
-const inputBox = document.getElementById("input-box");
-const listContainer = document.getElementById("List-container");
-function addTask(){
-    if(inputBox.value === '')
-        alert("You must write something!");
-    else{
-        let li = document.createElement("li");
-        li.innerHTML = inputBox.value;
-        listContainer.appendChild(li);
-        let span = document.createElement("span");
-        span.innerHTML = "\u00d7";
-        li.appendChild(span);
-    }
-    inputBox.value = "";
-    saveData();
-}
-
-listContainer.addEventListener("click", function(e){
-    if(e.target.tagName === "LI"){
-        e.target.classList.toggle("checked");
-    }else if(e.target.tagName === "SPAN"){
-        e.target.parentElement.remove();
-    }
-}, false);
-
-function saveData(){
-    localStorage.setItem("data", listContainer.innerHTML);
-}
-
-function showTask(){
-    listContainer.innerHTML = localStorage.getItem("data");
-}
-
-showTask();
-
-inputBox.addEventListener("keyup", function(event) {
-    if (event.key === "Enter") {
-      event.preventDefault();
-      addTask();
-    }
->>>>>>> 9cd24a0 (pushing)
 });
